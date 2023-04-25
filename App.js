a9ad0e7ed953cd64c2e06a53fa21bcca1be941d1
vasanth-LCU <129722526+vasanth-LCU@users.noreply.github.com>
2023-04-25 11:50:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Extrabit from './components/extrabit';
+
+
+function App() {
+  return (
+    <div className='App'>
+    <header>NEW QUOTES</header>
+    <Extrabit/>
+    <footer>by<br></br>DEENANATH CM </footer>
+    </div>
+    
+  );
+}
+
+export default App;
